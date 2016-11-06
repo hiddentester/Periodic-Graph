d89a79f83d1fb35bcead39ff2d36009c62f6d3d8
Hiddentester <jonathanchung115@hotmail.com>
2016-11-05 21:54:25 -0400

    Added data files and patched empty graphs

--- PeriodicGraph.java
+++ PeriodicGraph.java
@@ -1,7 +1,7 @@
 /****************************************
-	File Name:	PeriodicGraph.java
-	Name:		hiddentester
-	Version:	1.0.161105
+	File Name:		PeriodicGraph.java
+	Name:				hiddentester
+	Version:			1.0.16110502
 	Description:	This program runs code.
  ****************************************/
 
@@ -14,8 +14,8 @@ import java.awt.event.*;
 public class PeriodicGraph {
 	//Program information
 	private static final String TITLE = "Periodic Graph";
-	private static final String VERSION = "1.0.161105";
-	private static final String[] inputFiles = {"Ionization Energies.txt", "Atomic Radii.txt"};
+	private static final String VERSION = "1.0.16110502";
+	private static final int ELEMENTS = 118;
 	private double maxY;
 	
 	//Window scaling variables
@@ -36,9 +36,9 @@ public class PeriodicGraph {
 	   frame.add(draw);
 	   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	   frame.setSize(800, 600);
-		frame.setMinimumSize(new Dimension(600, 450));
+		frame.setMinimumSize(new Dimension(300, 225));
 	   frame.setVisible(true);
-		frame.setBackground(Color.black);
+		frame.setBackground(Color.white);
 		frame.setAlwaysOnTop(false);
 
 	   //Initialize title bar
@@ -52,45 +52,48 @@ public class PeriodicGraph {
 	//Drawing class
 	class Drawing extends JComponent {			
 		public void paint(Graphics g) {
-	 		//Update scaling factors based on the size of the window
-	 		xScale = (getWidth() - 2 * PADDING) / 118.0;
-	 		yScale = (getHeight() - 2 * PADDING - 50) / maxY;
-			
 			//Fill background
-			g.setColor(Color.black);
-	 	  	g.fillRect(0, 0, getWidth(), getHeight());
+				g.setColor(Color.black);
+		 	  	g.fillRect(0, 0, getWidth(), getHeight());
 			
-			//Draw axes
-			g.setColor(Color.red);
-			g.drawLine(PADDING,
-				getHeight() - 2 * PADDING,
-				getWidth() - 2 * PADDING,
-				getHeight() - 2 * PADDING);
-			g.drawLine(PADDING,
-				PADDING,
-				PADDING,
-				getHeight() - 2 * PADDING);
-			
-		  	//Draw data points
-		  	for (int i = 0; i < data.size(); i++) {
-				if ((double)data.get(i) != -1) {
-					//Draw lines
-					g.setColor(Color.green);
-					if (i > 0 && (double)data.get(i - 1) != -1.0) {
-						g.drawLine(PADDING + (int)((i) * xScale),
-							(int)(getHeight() - (2 * PADDING) - (double)data.get(i) * yScale),
-							PADDING + (int)((i - 1) * xScale),
-							(int)(getHeight() - (2 * PADDING) - (double)data.get(i - 1) * yScale));
-					} //if structure
-					
-					//Draw points
-					g.setColor(Color.yellow);
-					g.fillRect(PADDING + (int)((i) * xScale) - 2,
-						(int)(getHeight() - (2 * PADDING) - (double)data.get(i) * yScale) - 2,
-						4,
-						4);
-				}//if structure
-		 	} //for loop
+			//Draw graph
+			if (data != null && !data.isEmpty()) {
+		 		//Update scaling factors based on the size of the window
+		 		xScale = (getWidth() - 2 * PADDING) / (double)ELEMENTS;
+		 		yScale = (getHeight() - 2 * PADDING - 50) / maxY;
+				
+				//Draw axes
+				g.setColor(Color.red);
+				g.drawLine(PADDING,
+					getHeight() - 2 * PADDING,
+					getWidth() - 2 * PADDING,
+					getHeight() - 2 * PADDING);
+				g.drawLine(PADDING,
+					PADDING,
+					PADDING,
+					getHeight() - 2 * PADDING);
+				
+			  	//Draw data points
+			  	for (int i = 0; i < data.size(); i++) {
+					if ((double)data.get(i) != -1) {
+						//Draw lines
+						g.setColor(Color.green);
+						if (i > 0 && (double)data.get(i - 1) != -1.0) {
+							g.drawLine(PADDING + (int)((i) * xScale),
+								(int)(getHeight() - (2 * PADDING) - (double)data.get(i) * yScale),
+								PADDING + (int)((i - 1) * xScale),
+								(int)(getHeight() - (2 * PADDING) - (double)data.get(i - 1) * yScale));
+						} //if structure
+						
+						//Draw points
+						g.setColor(Color.yellow);
+						g.fillRect(PADDING + (int)((i) * xScale) - 2,
+							(int)(getHeight() - (2 * PADDING) - (double)data.get(i) * yScale) - 2,
+							4,
+							4);
+					}//if structure
+			 	} //for loop
+			} //if structure
 	   } // paint method
 	} // Drawing class
 	
@@ -100,7 +103,7 @@ public class PeriodicGraph {
 		data = new ArrayList();
 		
 		//Load data from file
-      try {
+		try {
 			in = new BufferedReader(new FileReader(dataSet + ".txt"));
 				
 	      while ((input = in.readLine()) != null) {
@@ -114,19 +117,23 @@ public class PeriodicGraph {
 				maxY = Math.max(maxY, (double)data.get(i));
 			} //for loop
 			
+			//Update Title
 			titleBar.setText(dataSet);
       } catch (Exception e) {
+			titleBar.setText(TITLE + " " + VERSION);
          System.err.println("There was a problem with the file.");
-         System.err.println("Exeception: " + e.getMessage());
+         System.err.println("Exception: " + e.getMessage());
       } //try-catch structure
 	} //loadData method
 	
 	//Main method
 	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
 	   PeriodicGraph graph = new PeriodicGraph();
-		PeriodicGraph graph2 = new PeriodicGraph();
 		
-		graph.loadData("Atomic Radii");
-		graph2.loadData("Ionization Energies");
+		while (true) {
+			graph.loadData(sc.nextLine());
+			graph.draw.repaint();
+		} //while loop
 	} // main method
 } // PeriodicGraph class
